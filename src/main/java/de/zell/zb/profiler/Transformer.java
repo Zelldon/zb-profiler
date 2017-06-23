4b6a6b6e34a5cd813e4666ffa8e73108bfef35c9
Christopher Zell <Zelldon91@gmail.com>
2017-06-23 14:47:09 +0200

    add javaassist solution

--- src/main/java/de/zell/zb/profiler/Transformer.java
+++ src/main/java/de/zell/zb/profiler/Transformer.java
@@ -1,63 +1,69 @@
 package de.zell.zb.profiler;
 
+import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
-import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
-import javassist.ClassPool;
+import javassist.*;
 
 /**
  *
  */
 public class Transformer implements ClassFileTransformer
 {
-    private final Instrumentation instrumentation;
     private final ClassPool classPool;
 
-    public Transformer(Instrumentation inst)
+    public Transformer()
     {
-        instrumentation = inst;
         classPool = ClassPool.getDefault();
-        instrumentation.addTransformer(this);
     }
 
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer)
         throws IllegalClassFormatException
     {
-        System.out.print("Loading class: ");
-        System.out.println(className);
-        return classfileBuffer;
-//        try
-//        {
-//            classPool.insertClassPath(new ByteArrayClassPath(className, classfileBuffer));
-//            final CtClass cc = classPool.get(className);
-//            final CtMethod[] methods = cc.getMethods();
-//            for (int k = 0; k < methods.length; k++)
-//            {
-//                if (methods[k].getLongName().startsWith(className))
-//                {
-//                    methods[k].insertBefore("System.out.println(\"Entering " + methods[k].getLongName() + "\");");
-//                    methods[k].insertAfter("System.out.println(\"Exiting " + methods[k].getLongName() + "\");");
-//                }
-//            }
-//
-//            // return the new bytecode array:
-//            final byte[] newClassfileBuffer = cc.toBytecode();
-//            return newClassfileBuffer;
-//        }
-//        catch (IOException ioe)
-//        {
-//            System.err.println(ioe.getMessage() + " transforming class " + className + "; returning uninstrumented    class");
-//        }
-//        catch (NotFoundException nfe)
-//        {
-//            System.err.println(nfe.getMessage() + " transforming class " + className + "; returning uninstrumented class");
-//        }
-//        catch (CannotCompileException cce)
-//        {
-//            System.err.println(cce.getMessage() + " transforming class " + className + "; returning uninstrumented class");
-//        }
-//        return null;
+        try
+        {
+            classPool.insertClassPath(new ByteArrayClassPath(className, classfileBuffer));
+            final String correctClassName = className.replace('/', '.');
+            final CtClass cc = classPool.get(correctClassName);
+            final CtMethod[] methods = cc.getMethods();
+            for (int k = 0; k < methods.length; k++)
+            {
+
+                final CtMethod method = methods[k];
+
+                if (method.getLongName().startsWith(correctClassName))
+                {
+                    method.addLocalVariable("_startTime", CtClass.longType);
+                    method.insertBefore("_startTime = System.nanoTime();");
+
+                    method.insertAfter("System.out.println(\"Executing: " +
+                                           method.getLongName() +
+                                           " takes \" + (System.nanoTime() - _startTime));");
+                }
+            }
+
+            // return the new bytecode array:
+            final byte[] newClassfileBuffer = cc.toBytecode();
+            return newClassfileBuffer;
+        }
+        catch (IOException ioe)
+        {
+            System.err.println(ioe.getMessage() + " transforming class " + className + "; returning uninstrumented    class");
+        }
+        catch (NotFoundException nfe)
+        {
+            System.err.println(nfe.getMessage() + " transforming class " + className + "; returning uninstrumented class");
+        }
+        catch (CannotCompileException cce)
+        {
+            System.err.println(cce.getMessage() + " transforming class " + className + "; returning uninstrumented class");
+        }
+        catch (Throwable t)
+        {
+            t.printStackTrace();
+        }
+        return null;
     }
 }
